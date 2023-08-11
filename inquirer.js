25ee791f015f7884e2a8a7bd7012d2e71d56413d
James Matthew Haynes <jamesmatthewhaynes@jamess-imac.myfiosgateway.com>
2023-08-11 12:41:01 -0400

    added inquirer prompt

--- inquirer.js
+++ inquirer.js
@@ -1,5 +1,53 @@
 const inquirer = require('inquirer');
 
+const questions = [
+  {
+    type:'list',
+    message: 'What would you like to do?',
+    name: 'options',
+    choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role'
+    ]
+  },
+  {
+    type: 'input',
+    message: 'Please enter a department to add to the database',
+    name: 'add_dept',
+    when(answers) {
+        return answers.options == 'Add a department';
+    },
+  },
+  {
+    type: 'input',
+    message: 'Please enter a role to add to the database',
+    name: 'add_role',
+    when(answers) {
+      return answers.options == 'Add a role';
+    },
+  },
+  {
+    type: 'input',
+    message: 'Please enter an employee to add to the database',
+    name: 'add_emp',
+    when(answers) {
+      return answers.options == 'Add an employee';
+    },
+  },
+  {
+    type: 'input',
+    message: 'Please enter the employee you would like to update',
+    name: 'update_emp',
+    when(answers) {
+      return answers.options == 'Update an employee role';
+    },
+  },
+];
 // what would you like to do?
 // view all departments
     //return table with dep name and id -- CHECK
@@ -20,11 +68,26 @@ const inquirer = require('inquirer');
 
 
 inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
+  .prompt(questions)
   .then((answers) => {
     // Use user feedback for... whatever!!
+    // console.log(answers.options);
+    // console.log(answers);
+    
+    switch() {
+      case :
+        console.log();
+        break;
+      case :
+        console.log();
+        break;
+      case :
+        console.log();
+        break;
+      case :
+        console.log();
+        break;
+    }
   })
   .catch((error) => {
     if (error.isTtyError) {
